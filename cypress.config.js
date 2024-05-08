8046d45b1955a7a94d48a0f3499669427026b2b0
Patricia Costa <patiii.costa@gmail.com>
2024-05-07 22:30:33 -0300

    Configura projeto

---
+++ cypress.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require('cypress');
+const {
+  addCucumberPreprocessorPlugin,
+} = require('@badeball/cypress-cucumber-preprocessor');
+const {
+  createEsbuildPlugin,
+} = require('@badeball/cypress-cucumber-preprocessor/esbuild');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://rarocrud-frontend-88984f6e4454.herokuapp.com",
+    env: {
+      apiBaseUrl: "https://rarocrud-80bf38b38f1f.herokuapp.com/api/v1",
+      TAGS: "not @ignore"
+    },
+    specPattern: 'cypress/e2e/**/*.feature',
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        'file:preprocessor',
+        createBundler({ plugins: [createEsbuildPlugin(config)] })
+      );
+
+      return config;
+    },
+  },
+});
