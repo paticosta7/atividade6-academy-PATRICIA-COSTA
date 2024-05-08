6d5046df8d4a9eb4cf179efc96b7c165d896f6d8
Patricia Costa <patiii.costa@gmail.com>
2024-05-08 15:51:00 -0300

    Configura teste

--- cypress.config.js
+++ cypress.config.js
@@ -1,25 +1,25 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 const {
   addCucumberPreprocessorPlugin,
-} = require('@badeball/cypress-cucumber-preprocessor');
+} = require("@badeball/cypress-cucumber-preprocessor");
 const {
   createEsbuildPlugin,
-} = require('@badeball/cypress-cucumber-preprocessor/esbuild');
-const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://rarocrud-frontend-88984f6e4454.herokuapp.com",
     env: {
       apiBaseUrl: "https://rarocrud-80bf38b38f1f.herokuapp.com/api/v1",
-      TAGS: "not @ignore"
+      TAGS: "not @ignore",
     },
-    specPattern: 'cypress/e2e/**/*.feature',
+    specPattern: "cypress/e2e/**/*.feature",
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
 
       on(
-        'file:preprocessor',
+        "file:preprocessor",
         createBundler({ plugins: [createEsbuildPlugin(config)] })
       );
 
